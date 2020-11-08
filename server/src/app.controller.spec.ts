33a945fc02d2314fe79b3e7333f9152ca88f432c
Teo Jun Sheng, Pierre <junshengpierre@gmail.com>
2020-11-08 23:39:59 +0800

    change return message

--- server/src/app.controller.spec.ts
+++ server/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Catalog Management"', () => {
+      expect(appController.getHello()).toBe('Catalog Management');
     });
   });
 });
